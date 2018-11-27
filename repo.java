3d59870dd2c730283089abbd0aaed1f419fddfc4
nick chowa <ncchowa@aggies.ncat.edu>
2018-11-27 18:24:42 -0500

    added javadoc

--- repo.java
+++ repo.java
@@ -1,4 +1,13 @@
+/**
+* Program prints out "goodbye comp 163" into the command bar.
+* @author ncchowa@aggies.ncat.edu
+*/
+
 public class repo {
+   /**
+   * main method.
+   * @param args The arguments for the main.
+   */
    public static void main(String[] args) {
       System.out.println("Goodbye COMP 163.");
    }
